be3a1e56cee255236fea77f0be05b8853632eabc
noamgrosman <noam074@gmail.com>
2026-05-25 14:30:04 +0300

    Final commit, can submit

--- ex1/ex1.cpp
+++ ex1/ex1.cpp
@@ -5,20 +5,7 @@
 #include <algorithm>
 #include <map>
 
-struct RoutineData {
-    std::string imageName;
-    ADDRINT imageAddress;
-    std::string routineName;
-    ADDRINT routineAddress;
-    UINT64 instructionCount;
-    UINT64 callCount;
-};
-
-static RoutineData routinesArray[1000];
-static int totalRoutinesFound = 0;
-static std::map<ADDRINT, RoutineData*> rtnAddrToData;
-
-//register tracking
+// register tracking
 static const int NUM_TRACKED_REGS = 6;
 static const REG TRACKED_REGS[NUM_TRACKED_REGS] = {
     LEVEL_BASE::REG_RAX, LEVEL_BASE::REG_RBX, LEVEL_BASE::REG_RCX,
@@ -32,8 +19,26 @@ static const int MAX_REG_SAMPLES = 20;
 struct RegSamples {
     ADDRINT values[MAX_REG_SAMPLES];
     int count;
+    RegSamples() : count(0) {}
 };
-static RegSamples regSamples[NUM_TRACKED_REGS];
+
+struct RoutineData {
+    std::string imageName;
+    ADDRINT imageAddress;
+    std::string routineName;
+    ADDRINT routineAddress;
+    UINT64 instructionCount;
+    UINT64 callCount;
+    RegSamples regs[NUM_TRACKED_REGS];
+
+    RoutineData()
+        : imageAddress(0), routineAddress(0),
+          instructionCount(0), callCount(0) {}
+};
+
+static RoutineData routinesArray[1000];
+static int totalRoutinesFound = 0;
+static std::map<ADDRINT, RoutineData*> rtnAddrToData;
 
 VOID CountRoutineCall(UINT64* callCountPtr) {
     (*callCountPtr)++;
@@ -43,9 +48,9 @@ VOID CountBblInstructions(UINT64* instructionCountPtr, UINT32 numIns) {
     (*instructionCountPtr) += numIns;
 }
 
-VOID RecordRegValue(UINT32 idx, ADDRINT val) {
-    if (regSamples[idx].count < MAX_REG_SAMPLES) {
-        regSamples[idx].values[regSamples[idx].count++] = val;
+VOID RecordRegValue(RegSamples* s, ADDRINT val) {
+    if (s->count < MAX_REG_SAMPLES) {
+        s->values[s->count++] = val;
     }
 }
 
@@ -94,17 +99,17 @@ VOID Trace(TRACE trace, VOID* v) {
                            IARG_PTR, &(data->instructionCount),
                            IARG_UINT32, BBL_NumIns(bbl),
                            IARG_END);
-        }
 
-        //instrument writes to tracked registers
-        for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins)) {
-            if (!INS_HasFallThrough(ins)) continue;
-            for (int i = 0; i < NUM_TRACKED_REGS; i++) {
-                if (INS_RegWContain(ins, TRACKED_REGS[i])) {
-                    INS_InsertCall(ins, IPOINT_AFTER, (AFUNPTR)RecordRegValue,
-                                   IARG_UINT32, (UINT32)i,
-                                   IARG_REG_VALUE, TRACKED_REGS[i],
-                                   IARG_END);
+            // per-routine register write tracking
+            for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins)) {
+                if (!INS_HasFallThrough(ins)) continue;
+                for (int i = 0; i < NUM_TRACKED_REGS; i++) {
+                    if (INS_RegWContain(ins, TRACKED_REGS[i])) {
+                        INS_InsertCall(ins, IPOINT_AFTER, (AFUNPTR)RecordRegValue,
+                                       IARG_PTR, &(data->regs[i]),
+                                       IARG_REG_VALUE, TRACKED_REGS[i],
+                                       IARG_END);
+                    }
                 }
             }
         }
@@ -115,39 +120,40 @@ VOID FINI(INT32 code, VOID* v) {
     std::sort(routinesArray, routinesArray + totalRoutinesFound, CompareRoutines);
     std::ofstream outFile("rtn-output.csv");
 
-    //per-routine info
     for (int i = 0; i < totalRoutinesFound; i++) {
-        if (routinesArray[i].instructionCount > 0 || routinesArray[i].callCount > 0) {
-            outFile << routinesArray[i].imageName << ", "
-                    << "0x" << std::hex << routinesArray[i].imageAddress << std::dec << ", "
-                    << routinesArray[i].routineName << ", "
-                    << "0x" << std::hex << routinesArray[i].routineAddress << std::dec << ", "
-                    << routinesArray[i].instructionCount << ", "
-                    << routinesArray[i].callCount << std::endl;
-        }
-    }
+        RoutineData& r = routinesArray[i];
+        if (r.instructionCount == 0 && r.callCount == 0) continue;
 
-    //per-register info
-    for (int i = 0; i < NUM_TRACKED_REGS; i++) {
-        const RegSamples& s = regSamples[i];
-        outFile << TRACKED_REG_NAMES[i] << " values: ";
-        for (int j = 0; j < s.count; j++) {
-            outFile << "0x" << std::hex << s.values[j] << std::dec;
-            if (j < s.count - 1) outFile << " -> ";
-        }
-        if (s.count >= 2) {
-            UINT64 sumAbsDelta = 0;
-            for (int j = 1; j < s.count; j++) {
-                INT64 d = (INT64)(s.values[j] - s.values[j - 1]);
-                sumAbsDelta += (d < 0) ? (UINT64)(-d) : (UINT64)d;
+        // routine row
+        outFile << r.imageName << ", "
+                << "0x" << std::hex << r.imageAddress << std::dec << ", "
+                << r.routineName << ", "
+                << "0x" << std::hex << r.routineAddress << std::dec << ", "
+                << r.instructionCount << ", "
+                << r.callCount << std::endl;
+
+        // 6 register lines under this routine
+        for (int j = 0; j < NUM_TRACKED_REGS; j++) {
+            const RegSamples& s = r.regs[j];
+            outFile << TRACKED_REG_NAMES[j] << " values: ";
+            for (int k = 0; k < s.count; k++) {
+                outFile << "0x" << std::hex << s.values[k] << std::dec;
+                if (k < s.count - 1) outFile << " -> ";
             }
-            UINT64 avg = sumAbsDelta / (s.count - 1);
-            outFile << "   Has an Average delta: Yes   Average delta: 0x"
-                    << std::hex << avg << std::dec;
-        } else {
-            outFile << "   Has an Average delta: No";
+            if (s.count >= 2) {
+                UINT64 sumAbsDelta = 0;
+                for (int k = 1; k < s.count; k++) {
+                    INT64 d = (INT64)(s.values[k] - s.values[k - 1]);
+                    sumAbsDelta += (d < 0) ? (UINT64)(-d) : (UINT64)d;
+                }
+                UINT64 avg = sumAbsDelta / (s.count - 1);
+                outFile << "  Has an Average delta: Yes  Average delta: 0x"
+                        << std::hex << avg << std::dec;
+            } else {
+                outFile << "  Has an Average delta: No";
+            }
+            outFile << std::endl;
         }
-        outFile << std::endl;
     }
     outFile.close();
 }
--- ex1_submit/src/README.txt
+++ ex1_submit/src/README.txt
@@ -1,8 +1,15 @@
 Name: Noam Grosman
 ID: 318677341
+
+Helper before running:
+	Set PIN_ROOT to the Pin kit root directory before building:
+		export PIN_ROOT=/path/to/pin-external-4.0-99633-g5ca9893f2-gcc-linux
+
 Compilation:
 	make PIN_ROOT=$PIN_ROOT obj-intel64/ex1.so
+
 Run:
 	$PIN_ROOT/pin -t obj-intel64/ex1.so -- ./bzip2 -k -f input.txt
+
 Output:
-	Produces rtn-output.csv in the directory.
+	Produce rtn-output.csv in the current directory.
